0d9df9c846e93ad930f1eb1ced61465fe49ab884
TheoLechemia <lechemia.theo@gmail.com>
2017-06-19 18:56:10 +0000

    up

--- Apps/addObs/static/addObs.js
+++ Apps/addObs/static/addObs.js
@@ -189,6 +189,31 @@ var selectedStyle = {
    'weight':3
   }
 
+    this.layers = {
+        baselayers: {
+            osm: {
+                name: "OpenStreetMap",
+                type: "xyz",
+                url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                layerOptions: {
+                    subdomains: ["a", "b", "c"],
+                    attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
+                    continuousWorld: true
+                }
+            },
+            cycle: {
+                name: "OpenCycleMap",
+                type: "xyz",
+                url: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+                layerOptions: {
+                    subdomains: ["a", "b", "c"],
+                    attribution: "&copy; <a href=\"http://www.opencyclemap.org/copyright\">OpenCycleMap</a> contributors - &copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
+                    continuousWorld: true
+                }
+            }
+        }
+    }
+
 
 
   var saveGeojsonMaille = {};
