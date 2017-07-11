89b06d2ad437a242a27c7bd056ba34a814950742
TheoLechemia <lechemia.theo@gmail.com>
2017-07-11 20:06:03 +0000

    halleluhia

--- Apps/download/ogrUtils.py
+++ Apps/download/ogrUtils.py
@@ -40,49 +40,24 @@ def pg2shp(table, outputPath, sql=None):
             fields.append(fieldDef.GetName())
             outLayer.CreateField(fieldDef)
 
-
         outLayerDefn = outLayer.GetLayerDefn()
         inLayerDefn = layer.GetLayerDefn()
 
-
         layer.ResetReading()
 
-        print 'NB featureeeee', layer.GetFeatureCount()
 
         for i in range(0,layer.GetFeatureCount()):
             inFeature = layer.GetFeature(i)
+            outFeature = ogr.Feature(inLayerDefn)
             for j in range(0, inLayerDefn.GetFieldCount()):
-                print >> sys.stderr, 'INNNNNNNNNNN',inLayerDefn.GetFieldDefn(j).GetNameRef()
-                print >> sys.stderr, 'OUTTTTTTTTTTTT', outLayerDefn.GetFieldDefn(j).GetNameRef()
-                indexIn = inFeature.GetFieldIndex(inLayerDefn.GetFieldDefn(j).GetNameRef())
-                indexOut = inFeature.GetFieldIndex(outLayerDefn.GetFieldDefn(j).GetNameRef())
-                print >> sys.stderr, 'INDEX INNNNN', indexIn 
-                print >> sys.stderr, "INDEX OUTTTTTTTT", indexOut 
+                outFeature.SetField(inLayerDefn.GetFieldDefn(j).GetNameRef(), inFeature.GetField(j))
 
-        # for i in range(0,layer.GetFeatureCount()):
-        #     inFeature = layer.GetFeature(i)
-        #     #create a feature in output
-        #     outFeature = ogr.Feature(outLayerDefn)
-        #     # loop over fields of each feature
-        #     for i in range(0, outLayerDefn.GetFieldCount()):
-        #         #all regular fields
-        #         print >> sys.stderr, "DEBUUUUUUUUUUUUUUUUUUUUUUUUUG"
-        #         print >> sys.stderr, inLayerDefn.GetFieldDefn(i).GetNameRef()
-        #         #print >> sys.stderr, inFeature.GetField(i)
-        #         index = inFeature.GetFieldIndex(inLayerDefn.GetFieldDefn(i).GetNameRef())
-        #         #print >> sys.stderr, index
-        #         if index != -1:
-        #             print >> sys.stderr, inFeature.GetField(index)
-        #         else:
-        #             print >> sys.stderr, inLayerDefn.GetFieldDefn(i).GetNameRef()
-        #         #outFeature.SetField(outLayerDefn.GetFieldDefn(i).GetNameRef(),inFeature.GetField(index))
-        #     #geom field
-        #     geom = inFeature.GetGeometryRef()
-        #     outFeature.SetGeometry(geom.Clone())
-        #     outLayer.CreateFeature(outFeature)
+            outFeature.SetGeometry(geom.Clone())
+            outLayer.CreateFeature(outFeature)
 
     conn.Destroy()
     outDs.Destroy()
+
 
 
 
