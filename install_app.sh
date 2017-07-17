83e18fdd64280569aea27b61a9011adc7ba92b1a
Ubuntu <ubuntu@serveur-1.localdomain>
2017-07-17 21:31:56 +0000

    up script global d'install

--- install_app.sh
+++ install_app.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 
-## Install de usershub
+# ## Install de usershub
 
-cp settings.ini.sample settings.ini
+# #cp settings.ini.sample settings.ini
 
 nano settings.ini
 
@@ -14,12 +14,25 @@ if [ "$(id -u)" == "0" ]; then
    exit 1
 fi
 
+function database_exists () {
+    # /!\ Will return false if psql can't list database. Edit your pg_hba.conf
+    # as appropriate.
+    if [ -z $1 ]
+        then
+        # Argument is null
+        return 0
+    else
+        # Grep db name in the list of database
+        sudo -n -u postgres -s -- psql -tAl | grep -q "^$1|"
+        return $?
+    fi
+}
 
 
 
 echo "Instalation de l'environnement logiciel..."
 #Instalation environnement PHP 
-sudo apt-get purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
+sudo apt-get -y purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
 
 sudo add-apt-repository ppa:ondrej/php
 
@@ -65,7 +78,7 @@ sudo -n -u postgres -s psql -c "CREATE ROLE $user_atlas WITH LOGIN PASSWORD '$us
 
 
 
-# Installation de UsersHub avec l'utilisateur courant
+# # Installation de UsersHub avec l'utilisateur courant
 echo "téléchargement et installation de UsersHub ..."
 cd /tmp
 wget https://github.com/PnEcrins/UsersHub/archive/$usershub_release.zip
@@ -101,9 +114,9 @@ echo "            ,\"pass\":\"$user_pg_pass\"" >> config/dbconnexions.json
 echo "            ,\"port\":\"$pg_port\"" >> config/dbconnexions.json 
 echo "        }" >> config/dbconnexions.json
 echo "        ,{" >> config/dbconnexions.json
-echo "            \"dbfunname\":\"GeoNature\"" >> config/dbconnexions.json 
+echo "            \"dbfunname\":\"BDN\"" >> config/dbconnexions.json 
 echo "            ,\"host\":\"$pg_host\"" >> config/dbconnexions.json 
-echo "            ,\"dbname\":\"$geonaturedb_name\"" >> config/dbconnexions.json 
+echo "            ,\"dbname\":\"$bdn_db_name\"" >> config/dbconnexions.json 
 echo "            ,\"user\":\"$user_pg\"" >> config/dbconnexions.json 
 echo "            ,\"pass\":\"$user_pg_pass\"" >> config/dbconnexions.json 
 echo "            ,\"port\":\"$pg_port\"" >> config/dbconnexions.json 
@@ -112,42 +125,177 @@ echo "    ]" >> config/dbconnexions.json
 echo "}" >> config/dbconnexions.json
 
 
+# ## creation de la vue pour utilisation des utiliateurs dans toutes les applications
 
-#Instalation de BDN
-
-# virtualenv ./venv
-# . ./venv/bin/activate
-# pip install -r requirements.txt
+# # export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/usershub.sql  &>> log/install_db.log
+# # export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/create_view_utilisateurs.sql  &>> log/install_db.log
 
 
 
+# #Instalation de BDN
+
+
+# ##INSTAL des dependances python
+# # virtualenv ./venv
+# # . ./venv/bin/activate
+# # pip install -r requirements.txt
+
+if database_exists $bdn_db_name
+then
+        if $drop_bdn_db
+            then
+            echo "Suppression de la BDD..."
+            sudo -n -u postgres -s dropdb $bdn_db_name  &>> logs/install_db.log
+        else
+            echo "La base de données existe et le fichier de settings indique de ne pas la supprimer."
+        fi
+fi
+
+# # Sinon je créé la BDD
+if ! database_exists $bdn_db_name 
+then
+  
+echo "Création de la BDD..."
+
+  sudo -u postgres psql -c "CREATE USER $user_pg WITH PASSWORD '$user_pg_pass'"
+
+  sudo -n -u postgres -s createdb -O $user_pg $bdn_db_name
+
+  echo "Ajout de postGIS et pgSQL à la base de données"
+  sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+
+  sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE SCHEMA contact_flore AUTHORIZATION $user_pg;"
+  sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE SCHEMA contact_faune AUTHORIZATION $user_pg;"
+  sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE SCHEMA synthese AUTHORIZATION $user_pg;"
+  #sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE SCHEMA taxonomie AUTHORIZATION $user_pg;"
+  sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE SCHEMA layers AUTHORIZATION $user_pg;"
+  sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE SCHEMA fdw AUTHORIZATION $user_pg;"
+  sudo -n -u postgres -s psql -d $bdn_db_name -c "CREATE SCHEMA utilisateur AUTHORIZATION $user_pg;"
+fi
 
 
 
 
+# ##################################
+# ### INSTALATION DE TAXHUB ########
+# ##################################
+
+# # Installation de TaxHub avec l'utilisateur courant
+echo "Téléchargement et installation de TaxHub ..."
+cd /tmp
+wget https://github.com/PnX-SI/TaxHub/archive/$taxhub_release.zip
+unzip $taxhub_release.zip
+rm $taxhub_release.zip
+mv TaxHub-$taxhub_release /home/$monuser/taxhub/
+cd /home/$monuser/taxhub
+
+# # Configuration des settings de TaxHub
+echo "Configuration de l'application TaxHub ..."
+cp settings.ini.sample settings.ini
+sed -i "s/drop_apps_db=.*$/drop_apps_db=false/g" settings.ini
+sed -i "s/db_host=.*$/db_host=$pg_host/g" settings.ini
+sed -i "s/db_port=.*$/db_port=$pg_port/g" settings.ini
+sed -i "s/db_name=.*$/db_name=$bdn_db_name/g" settings.ini
+sed -i "s/user_pg=.*$/user_pg=$user_pg/g" settings.ini
+sed -i "s/user_pg_pass=.*$/user_pg_pass=$user_pg_pass/g" settings.ini
+sed -i "s/user_schema=.*$/user_schema=local/g" settings.ini
+sed -i "s/usershub_host=.*$/usershub_host=$pg_host/g" settings.ini
+sed -i "s/usershub_port=.*$/usershub_port=$pg_port/g" settings.ini
+sed -i "s/usershub_db=.*$/usershub_db=$usershubdb_name/g" settings.ini
+sed -i "s/usershub_user=.*$/usershub_user=$user_pg/g" settings.ini
+sed -i "s/usershub_pass=.*$/usershub_pass=$user_pg_pass/g" settings.ini
 
 
 
 
+# # Configuration Apache de TaxHub
+sudo touch /etc/apache2/sites-available/taxhub.conf
+sudo sh -c 'echo "# Configuration TaxHub" >> /etc/apache2/sites-available/taxhub.conf'
+#sudo sh -c 'echo "RewriteEngine  on" >> /etc/apache2/sites-available/taxhub.conf'
+#sudo sh -c 'echo "RewriteRule    \"taxhub$\"  \"taxhub/\"  [R]" >> /etc/apache2/sites-available/taxhub.conf'
+sudo sh -c 'echo "<Location /taxhub>" >> /etc/apache2/sites-available/taxhub.conf'
+sudo sh -c 'echo "ProxyPass  http://127.0.0.1:8000/ retry=0" >> /etc/apache2/sites-available/taxhub.conf'
+sudo sh -c 'echo "ProxyPassReverse  http://127.0.0.1:8000/" >> /etc/apache2/sites-available/taxhub.conf'
+sudo sh -c 'echo "</Location>" >> /etc/apache2/sites-available/taxhub.conf'
+sudo sh -c 'echo "#FIN Configuration TaxHub" >> /etc/apache2/sites-available/taxhub.conf'
+
+sudo sed -i "s/<\/VirtualHost>//g" /etc/apache2/sites-available/000-default.conf
+sudo sed -i "s/# vim.*$//g" /etc/apache2/sites-available/000-default.conf
+sudo sh -c 'echo "# Configuration TaxHub - ne fonctionne pas dans le 000-default.conf" >> /etc/apache2/sites-available/000-default.conf'
+sudo sh -c 'echo "RewriteEngine  on" >> /etc/apache2/sites-available/000-default.conf'
+sudo sh -c 'echo "RewriteRule    \"taxhub$\"  \"taxhub/\"  [R]" >> /etc/apache2/sites-available/000-default.conf'
+sudo sh -c 'echo "</VirtualHost>" >> /etc/apache2/sites-available/000-default.conf'
+sudo sh -c 'echo "" >> /etc/apache2/sites-available/000-default.conf'
+sudo sh -c 'echo "# vim: syntax=apache ts=4 sw=4 sts=4 sr noet" >> /etc/apache2/sites-available/000-default.conf'
+
+sudo a2ensite taxhub
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+
+#Probleme avec le mod_rewrite
+sudo ls -l /usr/lib/apache2/modules/mod_rewrite.so
+sudo echo "LoadModule rewrite_module /usr/lib/apache2/modules/mod_rewrite.so" > /etc/apache2/mods-available/rewrite.load
+sudo a2enmod rewriteEnabling module rewrite
+sudo service apache2 restart
+
+# Installation et configuration de l'application TaxHub
+./install_app.sh
+# sudo apache2ctl restart
 
 
+## installation du schema taxonomie
 
 
+echo "Décompression des fichiers du taxref..."
+cd data/inpn
+unzip -o TAXREF_INPN_v9.0.zip -d /tmp
+unzip -o ESPECES_REGLEMENTEES.zip -d /tmp
+unzip -o LR_FRANCE.zip -d /tmp
+
+cd ../..
 
 
+echo "Création du schéma taxonomie..."
+echo "" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "Création du schéma taxonomie" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+export PGPASSWORD=$user_pg_pass;psql -h $pg_host -U $user_pg -d $bdn_db_name -f data/taxhubdb.sql  &>> logs/install_db.log
 
+echo "Insertion  des données taxonomiques de l'inpn... (cette opération peut être longue)"
+echo "" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "Insertion  des données taxonomiques de l'inpn" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+sudo -n -u postgres -s psql -d $bdn_db_name -f data/inpn/data_inpn_v9_taxhub.sql &>> logs/install_db.log
 
+echo "Création des données dictionnaires du schéma taxonomie..."
+echo "" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "Création des données dictionnaires du schéma taxonomie" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+export PGPASSWORD=$user_pg_pass;psql -h $pg_host -U $user_pg -d $bdn_db_name -f data/taxhubdata.sql  &>> logs/install_db.log
 
+echo "Insertion d'un jeu de taxons exemples dans le schéma taxonomie..."
+echo "" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "Insertion d'un jeu de taxons exemples dans le schéma taxonomie" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+export PGPASSWORD=$user_pg_pass;psql -h $pg_host -U $user_pg -d $bdn_db_name -f data/taxhubdata_taxon_example.sql  &>> logs/install_db.log
 
-
-
-cp ./Apps/config.py.sample ./Apps/config.py
-sudo nano ./Apps/config.py
-
-
-
-
-
-
-
-
+echo "Création de la vue représentant la hierarchie taxonomique..."
+echo "" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "Création de la vue représentant la hierarchie taxonomique" &>> logs/install_db.log
+echo "--------------------" &>> logs/install_db.log
+echo "" &>> logs/install_db.log
+export PGPASSWORD=$user_pg_pass;psql -h $pg_host -U $user_pg -d $bdn_db_name -f data/vm_hierarchie_taxo.sql  &>> logs/install_db.log
